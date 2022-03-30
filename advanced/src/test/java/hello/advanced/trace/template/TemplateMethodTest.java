d71a8c00ccca7c20201e12ad9fea1ba4b5a39f4a
Donggun.Chung <chungdk1117@gmail.com>
2022-03-30 20:53:05 +0900

    :hammer: 2022.03.30.Wed [Template Method Pattern]

---
+++ advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,69 @@
+package hello.advanced.trace.template;
+
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0 () {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+
+        log.info("BL logic1 start");
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+
+        log.info("BL logic2 start");
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+
+        log.info("resultTime={}", resultTime);
+    }
+
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        AbstractTemplate template2 = new SubClassLogic2();
+
+        template1.execute();
+        template2.execute();
+    }
+
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call () {
+                log.info("BL logic1 실행");
+            }
+        };
+        log.info("className = {}", template1.getClass());
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call () {
+                log.info("BL logic2 실행");
+            }
+        };
+        log.info("className = {}", template2.getClass());
+
+        template1.execute();
+        template2.execute();
+    }
+}
