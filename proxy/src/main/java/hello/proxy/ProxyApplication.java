45b82e680cb0857647b7cedfca32a6fbd5c9cb17
Donggun.Chung <chungdk1117@gmail.com>
2022-04-04 15:19:35 +0900

    :hammer: 2022.04.04.Mon [예제 프로젝트 생성 V1 ~ V3]

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,8 +1,13 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV3Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+//@Import(AppV1Config.class)
+//@Import(AppV2Config.class)
+@Import(AppV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
