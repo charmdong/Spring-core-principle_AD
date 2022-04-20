00d334fd12aaafa117d9a0e0af299f3d13ae56b1
Donggun.Chung <chungdk1117@gmail.com>
2022-04-20 21:54:44 +0900

    :hammer: 2022.04.20.Wed [후처리기]

---
+++ proxy/src/test/java/hello/proxy/postprocessor/BasicTest.java
@@ -0,0 +1,48 @@
+package hello.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+public class BasicTest {
+
+    @Test
+    void basicConfig() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(BasicConfig.class);
+
+        // A -> bean
+        A a = applicationContext.getBean("beanA", A.class);
+        a.helloA();
+
+        // B -> bean X
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean(B.class));
+    }
+
+    @Slf4j
+    @Configuration
+    static class BasicConfig {
+        @Bean(name = "beanA")
+        public A a () {
+            return new A();
+        }
+    }
+
+    @Slf4j
+    static class A {
+        public void helloA() {
+            log.info("Hello A");
+        }
+    }
+
+    @Slf4j
+    static class B {
+        public void helloB() {
+            log.info("Hello B");
+        }
+    }
+}
