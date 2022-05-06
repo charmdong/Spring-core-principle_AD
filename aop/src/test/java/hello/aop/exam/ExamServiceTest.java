0cd303cfc22d8737735502dbb9e5a3c9174c9fdc
Donggun.Chung <chungdk1117@gmail.com>
2022-05-06 21:35:58 +0900

    :hammer: 2022.05.06.Fri [Retry Aspect]

--- aop/src/test/java/hello/aop/exam/ExamServiceTest.java
+++ aop/src/test/java/hello/aop/exam/ExamServiceTest.java
@@ -1,5 +1,6 @@
 package hello.aop.exam;
 
+import hello.aop.exam.aop.RetryAspect;
 import hello.aop.exam.aop.TraceAspect;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -7,11 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @Slf4j
 @SpringBootTest
-@Import(TraceAspect.class)
+@Import({TraceAspect.class, RetryAspect.class})
 class ExamServiceTest {
 
     @Autowired
