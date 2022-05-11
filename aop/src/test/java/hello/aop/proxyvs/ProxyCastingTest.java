821e7ca54f4ee7daa237198fc36c4fbe42256821
Donggun.Chung <chungdk1117@gmail.com>
2022-05-12 00:42:44 +0900

    2022.05.12.Thu [cglib proxy casting]

--- aop/src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
+++ aop/src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
@@ -24,4 +24,17 @@ public class ProxyCastingTest {
             MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
         });
     }
+
+    @Test
+    void cglibProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true); // cglib proxy
+
+        // 프록시를 인터페이스로 캐스팅 성공
+        MemberService memberServiceProxy = (MemberService)proxyFactory.getProxy();
+
+        // cglib 프록시를 구현 클래스로 캐스팅 시도 성공
+        MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+    }
 }
